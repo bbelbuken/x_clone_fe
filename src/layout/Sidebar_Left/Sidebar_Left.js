fd66455648587b679fc7486bcbf251dbdc8668cd
Batuhan Belbüken <batuhan@PC192.168.1.8.station>
2024-12-09 01:10:42 +0300

    leftsidebar menu icons and names added

--- src/layout/Sidebar_Left/Sidebar_Left.js
+++ src/layout/Sidebar_Left/Sidebar_Left.js
@@ -1,9 +1,19 @@
-import Logo from './logo/Logo';
+import Logo from '../../utils/leftsidebarutils/logo/Logo';
+import { LeftSideBarUtils } from '../../utils/leftsidebarutils/LeftSideBarUtils';
+import { NavLink } from 'react-router-dom';
 
 const SidebarLeft = () => {
   return (
     <header className="min-h-screen w-[275px] px-2">
       <Logo />
+      <div>
+        {LeftSideBarUtils.map((menu, index) => (
+          <NavLink className="flex gap-8">
+            <p>{menu.icon.passive}</p>
+            <p>{menu.title}</p>
+          </NavLink>
+        ))}
+      </div>
     </header>
   );
 };
