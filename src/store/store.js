c59d6b271a4212c5965fa7099b5a058f05aa9245
Batuhan Belbüken <bbelbuken@gmail.com>
2024-12-23 18:52:42 +0300

    modalSlice and Hook added

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import accountReducer from '../features/account/accountSlice';
+import modalReducer from '../features/modal/modalSlice';
 
 export const store = configureStore({
   reducer: {
     accounts: accountReducer,
+    modal: modalReducer,
   },
 });
