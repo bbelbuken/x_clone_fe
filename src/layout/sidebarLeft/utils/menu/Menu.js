556d603aec55b7cebecabed625833f9b229598df
Batuhan Belbüken <bbelbuken@gmail.com>
2025-01-01 21:34:22 +0300

    menu bg color width fixed

--- src/layout/sidebarLeft/utils/menu/Menu.js
+++ src/layout/sidebarLeft/utils/menu/Menu.js
@@ -225,7 +225,7 @@ export const Menu = () => {
           to={`/${menu.path}`}
           className="flex w-full flex-grow items-start py-[4px]"
         >
-          <div className="transition-colors-custom flex h-auto max-w-full flex-grow items-center justify-start rounded-full p-3 hover:bg-[#e7e9ea1a]">
+          <div className="transition-colors-custom flex h-auto max-w-full items-center justify-start rounded-full p-3 hover:bg-[#e7e9ea1a]">
             <div className="relative h-auto">
               {`/${menu.path}` === location.pathname
                 ? menu.icon.active
