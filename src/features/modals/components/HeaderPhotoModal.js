559fdef81ba3ed2ead92e90cc14d5d76640fb2df
Batuhan Belbüken <bbelbuken@gmail.com>
2025-01-08 16:23:23 +0300

    header-photo not closes with everyclick but itself

--- src/features/modals/components/HeaderPhotoModal.js
+++ src/features/modals/components/HeaderPhotoModal.js
@@ -4,9 +4,9 @@ import { useAccounts } from 'hooks/useAccounts';
 
 const HeaderPhotoModal = ({ handleClose }) => {
   const { username } = useParams();
+  const modalRef = useRef(null);
   const accounts = useAccounts();
   const account = accounts.find((account) => account.username === username);
-  const modalRef = useRef(null);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -15,29 +15,35 @@ const HeaderPhotoModal = ({ handleClose }) => {
       }
     };
 
-    // Add event listener
     document.addEventListener('click', handleClickOutside);
 
-    // Clean up event listener on component unmount
     return () => {
       document.removeEventListener('click', handleClickOutside);
     };
   }, [handleClose]);
 
   return (
-    <div
-      className="fixed inset-0 z-50 my-auto flex max-h-[600px] items-center justify-center bg-black bg-opacity-70"
-      ref={modalRef}
-    >
+    <div className="fixed inset-0 z-50 my-auto flex max-h-[600px] items-center justify-center bg-black bg-opacity-70">
       <div className="flex">
-        <button
-          type="button"
-          onClick={() => handleClose()}
-          className="absolute right-4 top-4 text-white"
-        >
-          Close
-        </button>
+        <div className="flex min-h-8 min-w-[56px] items-center justify-start">
+          <button
+            type="button"
+            onClick={handleClose}
+            className="ml-[calc(-8px)] flex min-h-9 min-w-9 cursor-pointer items-center justify-center rounded-full outline-none transition-colors hover:bg-[#eff3f41a]"
+          >
+            <svg
+              viewBox="0 0 24 24"
+              fill="#eff3f4"
+              width={20}
+              height={20}
+              className=""
+            >
+              <path d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"></path>
+            </svg>
+          </button>
+        </div>
         <img
+          ref={modalRef}
           src={account.header_photo}
           alt="Header Fullscreen"
           className="max-h-full max-w-full object-cover"
--- src/pages/profile/profilebanner/ProfileBanner.js
+++ src/pages/profile/profilebanner/ProfileBanner.js
@@ -1,13 +1,25 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const ProfileBanner = ({ account }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const openPostModal = () => {
+    const previousRoute = location.pathname;
+    localStorage.setItem('previousRoute', previousRoute);
+    navigate('compose/post');
+  };
   return (
     <div className="relative">
       <Link
         to={`/${account.username}/header_photo`}
         className="absolute bottom-0 left-0 right-0 top-0 h-full w-full cursor-pointer transition-colors"
       >
-        <img src={account.header_photo} alt="header_photo" />
+        <img
+          src={account.header_photo}
+          alt="header_photo"
+          onClick={openPostModal}
+        />
       </Link>
     </div>
   );
