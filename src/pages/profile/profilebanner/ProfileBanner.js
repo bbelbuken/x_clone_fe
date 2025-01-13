23e243177f834c3381763d24ce2b72bd32c4beff
Batuhan Belbüken <bbelbuken@gmail.com>
2025-01-13 14:40:58 +0300

    header photo updated

--- src/pages/profile/profilebanner/ProfileBanner.js
+++ src/pages/profile/profilebanner/ProfileBanner.js
@@ -1,24 +1,9 @@
-import { useLocation, useNavigate } from 'react-router-dom';
+import HeaderPhoto from './headerphoto/HeaderPhoto';
 
 const ProfileBanner = ({ account }) => {
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  const handleHeaderPhotoClick = (e) => {
-    e.preventDefault();
-    navigate(`/${account.username}/header_photo`, {
-      state: { background: location },
-    });
-  };
-
   return (
-    <div className="relative">
-      <div
-        onClick={handleHeaderPhotoClick}
-        className="absolute bottom-0 left-0 right-0 top-0 h-full w-full cursor-pointer transition-colors"
-      >
-        <img src={account.header_photo} alt="header_photo" />
-      </div>
+    <div>
+      <HeaderPhoto account={account} />
     </div>
   );
 };
