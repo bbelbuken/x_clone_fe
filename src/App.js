347ab0781a34473e653a388ca760f087216ce4be
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-22 23:01:23 +0300

    created welcome component and change the main route

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <>
       <Routes location={background || location}>
-        <Route path="/" element={<Navigate to="/home" />} />
         {routes.map((route) => (
           <Route key={route.path} path={route.path} element={route.element}>
             {route.children?.map((child) => (
