683c71c930a6ccfad07ae1ac63951a66569f359c
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-23 21:59:58 +0300

    added twitter svg and improved welcome component

--- src/features/auth/Welcome.jsx
+++ src/features/auth/Welcome.jsx
@@ -4,24 +4,36 @@ import { Link } from 'react-router-dom';
 
 const Welcome = () => {
   return (
-    <main className="relative flex h-full flex-auto flex-col">
+    <main className="flex h-full flex-auto flex-col">
       <div className="flex min-h-auto flex-auto">
-        <div className="absolute top-0 right-0 bottom-0 left-0 flex min-h-45 flex-auto justify-center p-0">
-          <TwitterSVG className="max-h-[380px]" />
+        <div className="flex min-h-45 flex-auto justify-center p-0">
+          <TwitterSVG className="h-1/2 max-h-[380px] max-w-full p-6" />
         </div>
-        <div></div>
+
+        <div className="flex min-w-45 justify-center p-4">
+          <div className="w-full max-w-[760px] min-w-[437px] p-5">
+            <div className="my-12 min-w-0 text-[72px] leading-[77px] text-[#e7e9ea]">
+              <span className="font-twitter font-twitter-bold break-words">
+                Happening now
+              </span>
+            </div>
+            <div></div>
+          </div>
+        </div>
       </div>
 
       <div className="">
-        {FooterData.map((item, index) => (
-          <Link
-            key={index}
-            to={item.path}
-            className="my-0.5 min-w-0 cursor-pointer pr-3 text-[13px] leading-4 font-normal tracking-[0.015em] break-words text-[#71767b] hover:underline"
-          >
-            {item.title}
-          </Link>
-        ))}
+        <nav className="flex flex-wrap justify-center px-4 py-3">
+          {FooterData.map((item, index) => (
+            <Link
+              key={index}
+              to={item.path}
+              className="my-0.5 min-w-0 cursor-pointer pr-3 text-[13px] leading-4 font-normal tracking-[0.015em] break-words text-[#71767b] hover:underline"
+            >
+              {item.title}
+            </Link>
+          ))}
+        </nav>
       </div>
     </main>
   );
