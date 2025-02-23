eb8f49a99c78e0b22e3c1e6626e56a90c447ca69
Batuhan Belbüken <bbelbuken@hotmail.com>
2025-02-23 22:37:51 +0300

    added more data to footer and improved folder structure

---
+++ src/features/auth/welcome/Welcome.jsx
@@ -0,0 +1,43 @@
+import { TwitterSVG } from 'components/icons/TwitterSVG';
+import { Link } from 'react-router-dom';
+import { WelcomeFooterData } from './footerdata/WelcomeFooterData';
+
+const Welcome = () => {
+  return (
+    <main className="flex min-h-screen flex-auto flex-col">
+      <div className="flex min-h-auto flex-auto">
+        <div className="flex min-h-[45vh] flex-auto items-center justify-center p-0">
+          <TwitterSVG className="h-1/2 max-h-[380px] max-w-full p-6" />
+        </div>
+
+        <div className="item center flex min-w-[45vw] justify-center p-4">
+          <div className="flex w-full max-w-[760px] min-w-[437px] flex-col p-5">
+            <div className="my-12 flex min-w-0 text-[#e7e9ea]">
+              <span className="font-twitter font-twitter-bold text-[72px] leading-[77px] break-words">
+                Happening now
+              </span>
+            </div>
+
+            <div className="-">asdasd</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="">
+        <nav className="flex flex-wrap justify-center px-4 py-3">
+          {WelcomeFooterData.map((item, index) => (
+            <Link
+              key={index}
+              to={item.path}
+              className="my-1 min-w-0 cursor-pointer pr-4 text-[13px] leading-4 font-normal tracking-[0.025em] break-words text-[#71767b] hover:underline"
+            >
+              {item.title}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </main>
+  );
+};
+
+export default Welcome;
