9b27835dab4c71816026ac6ce44d7af56504a809
Batuhan Belbüken <bbelbuken@hotmail.com>
2025-02-23 21:07:23 +0300

    added footer to welcome page

--- src/features/auth/Welcome.jsx
+++ src/features/auth/Welcome.jsx
@@ -1,8 +1,17 @@
+import { FooterData } from 'layout/sidebarRight/utils/footer/footerdata/FooterData';
+import { Link } from 'react-router-dom';
+
 const Welcome = () => {
   return (
-    <div className="flex h-full shrink grow flex-col">
-      <div></div>
-      <div></div>
+    <div className="flex h-full flex-auto flex-col">
+      <div className="flex min-h-auto flex-auto"></div>
+      <div>
+        {FooterData.map((item, index) => (
+          <Link key={index} to={item.path} className="mb-0">
+            {item.title}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
