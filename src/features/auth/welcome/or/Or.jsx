23e53770d0d41c2cf814885ae978b39b54db7d31
Batuhan Belbüken <bbelbuken@hotmail.com>
2025-02-24 19:36:27 +0300

    improve code and added or section

---
+++ src/features/auth/welcome/or/Or.jsx
@@ -0,0 +1,13 @@
+const Or = () => {
+  return (
+    <div className="my-1 flex w-[300px] max-w-[380px]">
+      <div className="-mx-1 my-1 flex items-center justify-center">
+        <div className="m-w-0 mx-1 flex h-[1px] w-[135px] items-center justify-center bg-[#2f3336]"></div>
+        <div className="mx-1 text-[15px]">or</div>
+        <div className="m-w-0 mx-1 flex h-[1px] w-[135px] items-center justify-center bg-[#2f3336]"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Or;
