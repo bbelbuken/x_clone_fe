b23d642818ed8744846e2e95edcac1dce4c56da2
Batuhan Belbüken <bbelbuken@hotmail.com>
2025-02-25 17:31:55 +0300

    added dropdown svg and styled date inputs

---
+++ src/components/icons/DropDownSVG.jsx
@@ -0,0 +1,7 @@
+export const DropDownSVG = ({ className }) => {
+  return (
+    <svg viewBox="0 0 24 24" fill="#9ca3afaf" className={className}>
+      <path d="M3.543 8.96l1.414-1.42L12 14.59l7.043-7.05 1.414 1.42L12 17.41 3.543 8.96z"></path>
+    </svg>
+  );
+};
--- src/features/modals/components/createaccountmodal/formarea/dateinput/days/Days.jsx
+++ src/features/modals/components/createaccountmodal/formarea/dateinput/days/Days.jsx
@@ -1,28 +1,35 @@
 import { useState } from 'react';
+import { DropDownSVG } from 'components/icons/DropDownSVG';
 
-const Days = ({ daysInMonth }) => {
+const Days = ({ selectedDay, setSelectedDay, selectedMonth }) => {
   const [isFocusedDay, setIsFocusedDay] = useState(false);
 
   return (
     <label
       htmlFor="days"
-      className="relative mt-4 mb-3 flex h-full w-full flex-1 items-center justify-center rounded-[4px] border border-[#333639] px-2 py-3 pt-3 pb-2 focus-within:border-[#1d9bf0]"
+      className="relative mt-4 mr-3 mb-3 flex h-full flex-shrink-0 grow-3 items-center justify-center rounded-[4px] border border-[#333639] px-2 py-3 pt-3 pb-2 focus-within:border-[#1d9bf0]"
     >
       <div className="flex h-full justify-between">
         <div
-          className={`absolute top-1 transform text-[13px] leading-4 text-ellipsis transition-all ease-in-out select-none ${isFocusedDay ? 'text-[#1d9bf0]' : 'text-[#71767b]'}`}
+          className={`absolute top-2 transform text-[13px] leading-4 text-ellipsis transition-all ease-in-out select-none ${isFocusedDay ? 'text-[#1d9bf0]' : 'text-[#71767b]'}`}
         >
           <span>Day</span>
         </div>
       </div>
 
+      <DropDownSVG
+        className={`absolute top-4 right-3 h-[22.5px] w-[22.5px] transform text-[13px] leading-4 text-ellipsis transition-all ease-in-out select-none`}
+      />
+
       <select
         id="days"
-        className="box-border w-full min-w-0 appearance-none bg-transparent pt-3 text-left text-[17px] leading-6 text-[#e7e9ea] outline-none"
+        onChange={(e) => setSelectedDay(Number(e.target.value))}
+        className="box-border w-full min-w-0 appearance-none bg-black pt-3 text-left text-[17px] leading-6 text-[#e7e9ea] outline-none"
         onFocus={() => setIsFocusedDay(true)}
         onBlur={() => setIsFocusedDay(false)}
       >
-        {Array.from({ length: daysInMonth }, (_, index) => (
+        <option value=""></option>
+        {Array.from({ length: selectedDay }, (_, index) => (
           <option key={index} value={index + 1}>
             {index + 1}
           </option>
