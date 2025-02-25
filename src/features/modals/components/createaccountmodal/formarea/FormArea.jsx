4a815627bd8478d1cc2c054104d25534f7cda2c5
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-25 15:31:54 +0300

    added date section but still implementing

--- src/features/modals/components/createaccountmodal/formarea/FormArea.jsx
+++ src/features/modals/components/createaccountmodal/formarea/FormArea.jsx
@@ -1,11 +1,19 @@
 import NameInput from './nameinput.jsx/NameInput';
-import EmailInput from './emailInput/EmailInput';
+import EmailInput from './emailinput/EmailInput';
+import DateInput from './dateinput/DateInput';
 
 const FormArea = () => {
   return (
     <form className="flex h-full flex-1 flex-col px-20">
       <NameInput />
       <EmailInput />
+      <button
+        type="button"
+        className="cursor-pointer text-right text-[15px] leading-5 text-[#1d9bf0] hover:underline"
+      >
+        <span>Use phone instead</span>
+      </button>
+      <DateInput />
     </form>
   );
 };
