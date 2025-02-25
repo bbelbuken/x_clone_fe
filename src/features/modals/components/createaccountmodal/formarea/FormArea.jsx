e0fb2ca377e0e3bd04f242e9e8f59ff807e1dd60
Batuhan Belbüken <bbelbuken@hotmail.com>
2025-02-25 20:34:55 +0300

    we finished styling date inputs

--- src/features/modals/components/createaccountmodal/formarea/FormArea.jsx
+++ src/features/modals/components/createaccountmodal/formarea/FormArea.jsx
@@ -1,6 +1,7 @@
 import NameInput from './nameinput.jsx/NameInput';
 import EmailInput from './emailinput/EmailInput';
 import DateInput from './dateinput/DateInput';
+import Button from 'components/buttons/Button';
 
 const FormArea = () => {
   return (
@@ -14,6 +15,14 @@ const FormArea = () => {
         <span>Use phone instead</span>
       </button>
       <DateInput />
+      <Button
+        type="button"
+        className={
+          'my-6 mt-21 min-h-[52px] w-full bg-[#eff3f4] px-20 opacity-50 outline-none'
+        }
+      >
+        <span className="text-lg text-black">Next</span>
+      </Button>
     </form>
   );
 };
