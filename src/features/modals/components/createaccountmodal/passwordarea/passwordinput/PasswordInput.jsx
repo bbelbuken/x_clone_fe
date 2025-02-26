bca037f618e87823d2714cc1aab5f92f854ca57d
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-26 03:00:01 +0300

    password section added with styling

---
+++ src/features/modals/components/createaccountmodal/passwordarea/passwordinput/PasswordInput.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+
+const PasswordInput = ({ password, setPassword }) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [invalidPass, setInvalidPass] = useState(false);
+  const [active, setActive] = useState(false);
+
+  const handleInput = (e) => {
+    const inputValue = e.target.value;
+    setPassword(inputValue);
+    if (inputValue.length <= 8) {
+      setInvalidPass(true);
+      setActive(true);
+    } else {
+      setInvalidPass(false);
+      setActive(false);
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    const inputValue = e.target.value;
+    if (e.key === 'Backspace' && inputValue.length <= 8) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  };
+
+  return (
+    <label
+      htmlFor="password"
+      className={`relative mt-4 mb-3 flex h-full w-full flex-1 items-center justify-center rounded-[4px] border border-[#333639] px-2 py-3 pt-3 pb-2 ${invalidPass && active ? 'focus-within:border-[#f4212e]' : 'focus-within:border-[#1d9bf0]'}`}
+    >
+      <div className="flex h-full justify-between">
+        <div
+          className={`absolute text-ellipsis transition-all ease-in-out select-none ${
+            isFocused || password
+              ? 'top-1 transform text-[13px] leading-4'
+              : 'top-[30%] transform text-[17px] leading-6'
+          } ${isFocused ? 'text-[#1d9bf0]' : 'text-[#71767b]'} `}
+        >
+          <span>Password</span>
+        </div>
+      </div>
+
+      <input
+        type="password"
+        id="password"
+        value={password}
+        className="box-border w-full min-w-0 appearance-none bg-transparent pt-3 text-left text-[17px] leading-6 text-[#e7e9ea] outline-none"
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        onChange={handleInput}
+        onKeyDown={handleKeyDown}
+        autoComplete="off"
+      />
+    </label>
+  );
+};
+
+export default PasswordInput;
