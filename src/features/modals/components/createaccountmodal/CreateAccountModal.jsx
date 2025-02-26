0d2d1e8747dfb2ed8dd8cdab83b521643facba5c
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-26 15:26:31 +0300

    performance issues fixed, styles added to password section

--- src/features/modals/components/createaccountmodal/CreateAccountModal.jsx
+++ src/features/modals/components/createaccountmodal/CreateAccountModal.jsx
@@ -4,38 +4,38 @@ import FormArea from './formarea/FormArea';
 import PasswordArea from './passwordarea/PasswordArea';
 
 const CreateAccountModal = () => {
-  const [step, setStep] = useState(1);
-  const navigate = useNavigate();
+    const [step, setStep] = useState(1);
+    const navigate = useNavigate();
 
-  const handleNextSection = () => {
-    setStep(step + 1);
-  };
+    const handleNextSection = () => {
+        setStep(step + 1);
+    };
 
-  const handleClose = useCallback(() => {
-    const previousRoute = localStorage.getItem('previousRouteWelcomePage');
-    navigate(previousRoute || '/');
-  }, [navigate]);
+    const handleClose = useCallback(() => {
+        const previousRoute = localStorage.getItem('previousRouteWelcomePage');
+        navigate(previousRoute || '/');
+    }, [navigate]);
 
-  const handleClickOutside = (e) => {
-    if (e.target === e.currentTarget) {
-      handleClose();
-    }
-  };
+    const handleClickOutside = (e) => {
+        if (e.target === e.currentTarget) {
+            handleClose();
+        }
+    };
 
-  return (
-    <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-[#4a5c687c]"
-      onClick={handleClickOutside}
-    >
-      {step == 1 && (
-        <FormArea
-          handleClose={handleClose}
-          handleNextSection={handleNextSection}
-        />
-      )}
-      \{step == 2 && <PasswordArea handleClose={handleClose} />}
-    </div>
-  );
+    return (
+        <div
+            className="fixed inset-0 z-50 flex items-center justify-center bg-[#4a5c687c]"
+            onClick={handleClickOutside}
+        >
+            {step == 1 && (
+                <FormArea
+                    handleClose={handleClose}
+                    handleNextSection={handleNextSection}
+                />
+            )}
+            {step == 2 && <PasswordArea handleClose={handleClose} />}
+        </div>
+    );
 };
 
 export default CreateAccountModal;
