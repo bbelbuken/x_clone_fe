b37b52b14af146748e058de54e522adab358c3cc
Batuhan Belbüken <bbelbuken@hotmail.com>
2025-02-26 20:24:57 +0300

    we can succesfully upload and preview avatar photo while creating account

---
+++ src/features/modals/components/createaccountmodal/avatararea/AvatarArea.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import FormHeader from '../formarea/formheader/FormHeader';
+import Button from 'components/buttons/Button';
+import AvatarInput from './avatarinput/AvatarInput';
+
+const AvatarArea = ({ handleNextSection }) => {
+    const [media, setMedia] = useState(null);
+    return (
+        <div className="absolute top-[24.5%] left-[339px] z-50 mx-auto flex h-auto max-h-[90vh] w-full max-w-[40vw] min-w-[600px] flex-col rounded-2xl bg-black">
+            <FormHeader />
+
+            <div className="mt-[5px] flex h-full flex-1 flex-col px-20">
+                <div className="my-4">
+                    <h1 className="mb-[6px] min-w-0 text-[31px] leading-9 font-bold text-[#e7e9ea]">
+                        Pick a profile picture
+                    </h1>
+                    <span className="text-[15px] tracking-[0.013em] text-[#71767b]">
+                        Have a favorite selfie? Upload it now.
+                    </span>
+                </div>
+            </div>
+
+            <AvatarInput media={media} setMedia={setMedia} />
+
+            <div className="mt-82 min-w-0 px-20 text-[13px] leading-4 tracking-[0.010em] text-[#71767b]">
+                <Button
+                    type="button"
+                    className={`${media ? 'bg-[#fff]' : 'border border-[#71767b] bg-[#000] hover:bg-[#eff3f41a]'} my-6 mt-6 min-h-[52px] w-full px-20 outline-none`}
+                    onClick={media ? handleNextSection : null}
+                >
+                    <span className="text-lg text-[#fff]">Skip for now</span>
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default AvatarArea;
