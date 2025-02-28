36d33c2d02be6080205b0f98fe34e52d5f91bf23
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-28 16:45:26 +0300

    we implemented sign in first page

--- src/features/modals/components/signinmodal/signinoptions/SignInOptions.jsx
+++ src/features/modals/components/signinmodal/signinoptions/SignInOptions.jsx
@@ -4,6 +4,8 @@ import GoogleSignUp from 'features/auth/welcome/googlesignup/GoogleSignUp';
 import AppleSignUp from 'features/auth/welcome/applesignup/AppleSignUp';
 import Or from 'features/auth/welcome/or/Or';
 import UsernameOrEmailInput from './usernameoremailinput/UsernameOrEmailInput';
+import Button from 'components/buttons/Button';
+import { Link } from 'react-router-dom';
 
 const SignInOptions = ({
     handleClose,
@@ -15,7 +17,7 @@ const SignInOptions = ({
     setEmail,
 }) => {
     return (
-        <div className="absolute top-[24.5%] left-[339px] z-50 mx-auto flex h-auto max-h-[90vh] w-full max-w-[40vw] min-w-[600px] flex-col items-center justify-center rounded-2xl bg-black">
+        <div className="absolute top-[24.6%] left-[339px] z-50 mx-auto flex h-auto max-h-[90vh] w-full max-w-[40vw] min-w-[600px] flex-col items-center justify-center rounded-2xl bg-black">
             <FormHeader handleClose={handleClose} step={step} />
 
             <div className="mt-[5px] flex h-full max-w-[364px] min-w-[364px] flex-1 flex-col items-center justify-center px-8 pb-12">
@@ -38,6 +40,30 @@ const SignInOptions = ({
                     setEmail={setEmail}
                     handleNextSection={handleNextSection}
                 />
+                <div className="mt-[9px] flex h-[36px] w-[300px] items-center justify-center rounded-[20px] border border-[#536471] bg-transparent">
+                    <Button
+                        size="forgot-password"
+                        title="forgot password?"
+                        className="flex w-80 cursor-pointer items-center justify-center border-0 hover:bg-[#71767b46]"
+                    >
+                        <span className="ml-2 text-[15px] font-bold text-[#e7e9ea]">
+                            Forgot password?
+                        </span>
+                    </Button>
+                </div>
+
+                <div className="mt-[42px] mb-13 flex h-[36px] w-[300px] items-center justify-start rounded-[20px]">
+                    <span className="text-[15px] tracking-[0.010em] text-[#71767b]">
+                        Don&apos;t have an account?
+                        <Link
+                            to="/i/flow/signup"
+                            className="text-[#1d9bf0] hover:underline"
+                        >
+                            {' '}
+                            Sign up
+                        </Link>
+                    </span>
+                </div>
             </div>
         </div>
     );
