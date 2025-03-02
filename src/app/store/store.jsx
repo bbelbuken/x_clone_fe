40bdd9b38361ed190a3bacc75236fcf1171cd5ac
Batuhan Belbüken <bbelbuken@gmail.com>
2025-03-02 15:39:25 +0300

    created apiSlice

---
+++ src/app/store/store.jsx
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import accountReducer from '../../features/accounts/accountSlice';
+import modalReducer from '../../features/modals/modalSlice';
+import postReducer from '../../features/posts/postSlice';
+
+export const store = configureStore({
+    reducer: {
+        posts: postReducer,
+        accounts: accountReducer,
+        modals: modalReducer,
+    },
+});
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import { createRoot } from 'react-dom/client';
 import './assets/css/index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import { store } from './store/store.jsx';
+import { store } from './app/store/store.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>,
+    <StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </StrictMode>,
 );
