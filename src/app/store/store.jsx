8f1ae8e0929846cb8e6e1a87da6e0d534d7a2a49
Batuhan Belbüken <bbelbuken@gmail.com>
2025-03-08 21:05:49 +0300

    we implemented persisten with redux

--- src/app/store/store.jsx
+++ src/app/store/store.jsx
@@ -1,19 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import accountReducer from '../../features/accounts/accountSlice';
 import modalReducer from '../../features/modals/modalSlice';
 import postReducer from '../../features/posts/postSlice';
-import { apiSlice } from '../api/apiSlice';
 import authReducer from '../../features/auth/authSlice';
+import { apiSlice } from '../api/apiSlice';
+
+// Persist configuration
+const persistConfig = {
+    key: 'root', // Key for the persisted state
+    storage, // Storage engine (localStorage by default)
+    whitelist: ['auth'], // Only persist the 'auth' slice (or any other slices you want to persist)
+};
+
+// Wrap the auth reducer with persistReducer
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
-        auth: authReducer,
+
+        auth: persistedAuthReducer,
         posts: postReducer,
         accounts: accountReducer,
         modals: modalReducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(apiSlice.middleware),
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ['persist/PERSIST'], // Ignore redux-persist actions
+            },
+        }).concat(apiSlice.middleware),
     devTools: true,
 });
+
+// Create a persistor
+export const persistor = persistStore(store);
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,18 @@ import { createRoot } from 'react-dom/client';
 import './assets/css/index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import { store } from './app/store/store.jsx';
+import { persistor, store } from './app/store/store.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </PersistGate>
         </Provider>
     </StrictMode>,
 );
