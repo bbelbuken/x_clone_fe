327fe2e772d05577ae8fd6b6122a031596ec6ba4
Batuhan Belbüken <bbelbuken@gmail.com>
2025-03-25 00:44:37 +0300

    improved design and styling

--- src/layout/sidebarRight/utils/footer/Footer.jsx
+++ src/layout/sidebarRight/utils/footer/Footer.jsx
@@ -5,48 +5,53 @@ import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react';
 import { MoreButtonData } from './morebuttondata/MoreButtonData';
 
 export const Footer = () => {
-  const date = new Date();
-  const year = date.getFullYear();
+    const date = new Date();
+    const year = date.getFullYear();
 
-  return (
-    <nav className="relative flex flex-wrap items-center justify-start px-4">
-      {FooterData.map((item, index) => (
-        <Link
-          key={index}
-          to={item.path}
-          className="my-0.5 min-w-0 cursor-pointer pr-3 text-[13px] leading-4 font-normal tracking-[0.015em] break-words text-[#71767b] hover:underline"
-        >
-          {item.title}
-        </Link>
-      ))}
-      <Popover className={'absolute top-[17px] right-[83px]'}>
-        <PopoverButton>
-          <div className="flex items-center justify-center">
-            <span className="text-[13px] leading-4 tracking-[0.015em] text-[#71767b] hover:underline">
-              More
-            </span>
-            <svg viewBox="0 0 24 24" fill="#71767b" width={17} height={13}>
-              <path d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path>
-            </svg>
-          </div>
-        </PopoverButton>
-        <PopoverPanel>
-          <div className="max-h-[calc(-347.2px + 100vh)] box-shadow-morebox absolute top-[5px] -right-3 z-40 flex max-w-[calc(384px)] flex-col overflow-y-auto rounded-xl bg-black">
-            {MoreButtonData.map((item, index) => (
-              <Link
-                key={index}
-                to={item.path}
-                className="z-10 flex w-full min-w-0 flex-1 cursor-pointer px-4 py-[14px] text-[15px] leading-4 font-bold tracking-[0.010em] break-words whitespace-nowrap text-[#e7e9ea] transition-all hover:bg-[#16181c]"
-              >
-                {item.title}
-              </Link>
+    return (
+        <nav className="relative flex flex-wrap items-center justify-start px-4">
+            {FooterData.map((item, index) => (
+                <Link
+                    key={index}
+                    to={item.path}
+                    className="my-0.5 min-w-0 cursor-pointer pr-3 text-[13px] leading-4 font-normal tracking-[0.015em] break-words text-[#71767b] hover:underline"
+                >
+                    {item.title}
+                </Link>
             ))}
-          </div>
-        </PopoverPanel>
-      </Popover>
-      <div className="absolute top-11 left-4 z-0 flex items-center justify-center text-[13px] leading-3 tracking-[0.025em] text-[#71767b]">
-        <span>&copy; {year} X Corp</span>
-      </div>
-    </nav>
-  );
+            <Popover className={'absolute top-[17px] right-[83px]'}>
+                <PopoverButton>
+                    <div className="flex cursor-pointer items-center justify-center">
+                        <span className="text-[13px] leading-4 tracking-[0.015em] text-[#71767b] hover:underline">
+                            More
+                        </span>
+                        <svg
+                            viewBox="0 0 24 24"
+                            fill="#71767b"
+                            width={17}
+                            height={13}
+                        >
+                            <path d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path>
+                        </svg>
+                    </div>
+                </PopoverButton>
+                <PopoverPanel>
+                    <div className="max-h-[calc(-347.2px + 100vh)] box-shadow-morebox absolute top-[5px] -right-3 z-40 flex max-w-[calc(384px)] flex-col overflow-y-auto rounded-xl bg-black">
+                        {MoreButtonData.map((item, index) => (
+                            <Link
+                                key={index}
+                                to={item.path}
+                                className="z-10 flex w-full min-w-0 flex-1 cursor-pointer px-4 py-[14px] text-[15px] leading-4 font-bold tracking-[0.010em] break-words whitespace-nowrap text-[#e7e9ea] transition-all hover:bg-[#16181c]"
+                            >
+                                {item.title}
+                            </Link>
+                        ))}
+                    </div>
+                </PopoverPanel>
+            </Popover>
+            <div className="absolute top-11 left-4 z-0 flex items-center justify-center text-[13px] leading-3 tracking-[0.025em] text-[#71767b]">
+                <span>&copy; {year} X Corp</span>
+            </div>
+        </nav>
+    );
 };
--- src/layout/sidebarRight/utils/whatshappening/WhatIsHappening.jsx
+++ src/layout/sidebarRight/utils/whatshappening/WhatIsHappening.jsx
@@ -19,7 +19,7 @@ const WhatIsHappening = () => {
                         </Link>
 
                         <Popover>
-                            <PopoverButton className="absolute top-[6px] right-2 flex h-[34.75px] w-[34.75px] items-center justify-center rounded-full text-[#71767b] transition-colors hover:bg-[#1d9bf022] hover:text-[#1d9bf0]">
+                            <PopoverButton className="absolute top-[6px] right-2 flex h-[34.75px] w-[34.75px] cursor-pointer items-center justify-center rounded-full text-[#71767b] transition-colors hover:bg-[#1d9bf022] hover:text-[#1d9bf0]">
                                 <div className="flex items-center justify-center">
                                     <ThreeDotSVG />
                                 </div>
