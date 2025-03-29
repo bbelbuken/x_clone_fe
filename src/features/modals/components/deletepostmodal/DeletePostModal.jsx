53205c559b9809f5221709e6211ca944ac7d693b
Batuhan Belbüken <bbelbuken@gmail.com>
2025-03-29 21:05:28 +0300

    solved delete spinner not showing issue

--- src/features/modals/components/deletepostmodal/DeletePostModal.jsx
+++ src/features/modals/components/deletepostmodal/DeletePostModal.jsx
@@ -32,40 +32,45 @@ const DeletePostModal = () => {
     }
 
     return (
-        <div
-            onClick={handleClickOutside}
-            className="fixed inset-0 z-50 flex items-center justify-center bg-[#4a5c687c]"
-        >
-            {isLoading && <LoadingSpinner />}
+        <div>
+            {isLoading && (
+                <div>
+                    {' '}
+                    <LoadingSpinner />
+                </div>
+            )}
             <div
-                ref={modalRef}
-                className="z-50 mx-auto flex min-h-[calc(192px)] w-[320px] max-w-[80vh] flex-col rounded-2xl bg-black p-8"
+                onClick={handleClickOutside}
+                className="fixed inset-0 z-40 flex items-center justify-center bg-[#4a5c687c]"
             >
-                <h1 className="z-50text-left min-w-0 text-xl font-bold tracking-wide break-words">
-                    Delete post?
-                </h1>
-
-                <p className="mt-[5px] mb-0.5 w-full min-w-0 text-left text-[15px] leading-5 font-normal tracking-[0.010em] break-words text-[#71767b]">
-                    This can’t be undone and it will be removed from your
-                    profile, the timeline of any accounts that follow you, and
-                    from search results.
-                </p>
-
-                <div className="mt-6 mb-[calc(-12px)] flex flex-col transition-colors">
-                    <Button
-                        size="deletePost"
-                        onClick={handleDeletePost}
-                        className="mb-3 min-h-[44px] text-white hover:bg-[#dc1e29]"
-                    >
-                        Delete
-                    </Button>
-                    <Button
-                        size="profile-follow"
-                        onClick={handleClose}
-                        className="mb-3 min-h-[44px] hover:bg-[#eff3f41a]"
-                    >
-                        Cancel
-                    </Button>
+                <div
+                    ref={modalRef}
+                    className="z-50 mx-auto flex min-h-[calc(192px)] w-[320px] max-w-[80vh] flex-col rounded-2xl bg-black p-8"
+                >
+                    <h1 className="z-50text-left min-w-0 text-xl font-bold tracking-wide break-words">
+                        Delete post?
+                    </h1>
+                    <p className="mt-[5px] mb-0.5 w-full min-w-0 text-left text-[15px] leading-5 font-normal tracking-[0.010em] break-words text-[#71767b]">
+                        This can’t be undone and it will be removed from your
+                        profile, the timeline of any accounts that follow you,
+                        and from search results.
+                    </p>
+                    <div className="mt-6 mb-[calc(-12px)] flex flex-col transition-colors">
+                        <Button
+                            size="deletePost"
+                            onClick={handleDeletePost}
+                            className="mb-3 min-h-[44px] text-white hover:bg-[#dc1e29]"
+                        >
+                            Delete
+                        </Button>
+                        <Button
+                            size="profile-follow"
+                            onClick={handleClose}
+                            className="mb-3 min-h-[44px] hover:bg-[#eff3f41a]"
+                        >
+                            Cancel
+                        </Button>
+                    </div>
                 </div>
             </div>
         </div>
