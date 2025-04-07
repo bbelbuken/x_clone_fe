151a64a133642b7100cbfdf4b440acec81e1a340
Batuhan Belbüken <bbelbuken@gmail.com>
2025-04-07 23:23:59 +0300

    updated backend baseURL for deployment

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://x-clone-be-uvoa.onrender.com' }),
     credentials: 'include',
     tagTypes: ['Post', 'Account', 'Auth'],
     endpoints: (builder) => ({}),
