a907631a50e3f968d17728df6631451d80a82754
Batuhan Belbüken <bbelbuken@gmail.com>
2025-04-08 00:07:24 +0300

    hardcoded api url has changed to env

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://x-clone-be-uvoa.onrender.com' }),
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
     credentials: 'include',
     tagTypes: ['Post', 'Account', 'Auth'],
     endpoints: (builder) => ({}),
